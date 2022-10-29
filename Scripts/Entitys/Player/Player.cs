098cec5983bbd320de16ee55be76d5b2b8e8aea8
unknown <nazar.sheggy1@gmail.com>
2022-10-29 23:09:03 +0200

    v 1.01

--- Scripts/Entitys/Player/Player.cs
+++ Scripts/Entitys/Player/Player.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, IMove
 {
-    public void Move(float _speed, Transform _side, GameObject gmb)
-    { 
-        gmb.transform.position = Vector2.MoveTowards(gmb.transform.position, _side.position, _speed * Time.deltaTime);
+    public void Move(float _speed, Transform _side, GameObject _gmbForTransform)
+    {
+        _gmbForTransform.transform.position = Vector2.MoveTowards(_gmbForTransform.transform.position, _side.position, _speed * Time.deltaTime);
     }
     void Start()
     {
